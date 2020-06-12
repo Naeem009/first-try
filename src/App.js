adc2df82a7419b883b88125cbd5346b0c6fbb598
Naeem009 <56760480+Naeem009@users.noreply.github.com>
2020-06-12 12:37:58 +0500

    Some text updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World FROM Naeem Shahzad, Faisalabad</h1>
+      <h1>Hello World FROM Naeem Shahzad.</h1>
     </div>
   );
 }
